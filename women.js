b430b6393c985948964bb471ac8e7f350b4f918e
Fedi <Uifdotdfsot@gmail.com>
2023-08-24 11:56:57 +0100

    first commit

---
+++ women.js
@@ -0,0 +1,127 @@
+
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
+
+cartIcon.onclick = () => {
+    cart.classList.add("active");
+};
+
+closeCart.onclick = () => {
+    cart.classList.remove("active");
+};
+
+if (document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", ready);
+} else {
+    ready();
+}
+
+function ready() {
+    var removeCartButtons = document.getElementsByClassName("cart-remove");
+    for (var i = 0; i < removeCartButtons.length; i++) {
+        var button = removeCartButtons[i];
+        button.addEventListener("click", removeCartItem);
+    }
+
+    var quantityInputs = document.getElementsByClassName("cart-quantity");
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i];
+        input.addEventListener("change", quantityChanged);
+    }
+
+    var addCart = document.getElementsByClassName("add-cart");
+    for (var i = 0; i < addCart.length; i++) {
+        var button = addCart[i];
+        button.addEventListener("click", addCartClicked);
+    }
+
+    var buyButton = document.querySelector(".btn-buy");
+    buyButton.addEventListener("click", buyButtonClicked);
+}
+
+function buyButtonClicked() {
+    alert("Your Order is placed");
+    var cartContent = document.querySelector(".cart-content");
+    while (cartContent.hasChildNodes()) {
+        cartContent.removeChild(cartContent.firstChild);
+    }
+    updatetotal();
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+    updatetotal();
+}
+
+function quantityChanged(event) {
+    var input = event.target;
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+    updatetotal();
+}
+
+function addCartClicked(event) {
+    var button = event.target;
+    var productBox = button.closest(".product-box");
+    var title = productBox.querySelector(".product-title").innerText;
+    var price = productBox.querySelector(".price").innerText;
+    var productImg = productBox.querySelector(".product-img").src;
+    addProductToCart(title, price, productImg);
+    updatetotal();
+}
+
+function addProductToCart(title, price, productImg) {
+    var cartItems = document.querySelector(".cart-content");
+    var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+
+    for (var i = 0; i < cartItemsNames.length; i++) {
+        if (cartItemsNames[i].innerText === title) {
+            alert("You have already added this item to the cart");
+            return;
+        }
+    }
+
+    var cartShopBox = document.createElement("div");
+    cartShopBox.classList.add("cart-box");
+
+    var cartBoxContent = `
+        <img src="${productImg}" alt="" class="cart-img">
+        <div class="detail-box">
+            <div class="cart-product-title">${title}</div>
+            <div class="cart-price">${price}</div>
+            <input type="number" value="1" class="cart-quantity">
+        </div>
+        <i class='bx bxs-trash-alt cart-remove'></i> `;
+
+    cartShopBox.innerHTML = cartBoxContent;
+
+    cartShopBox.getElementsByClassName("cart-remove")[0].addEventListener("click", removeCartItem);
+    cartShopBox.getElementsByClassName("cart-quantity")[0].addEventListener("change", quantityChanged);
+
+    cartItems.appendChild(cartShopBox);
+
+    updatetotal();
+}
+
+function updatetotal() {
+    var cartContent = document.querySelector(".cart-content");
+    var cartBoxes = cartContent.getElementsByClassName("cart-box");
+    var total = 0;
+
+    for (var i = 0; i < cartBoxes.length; i++) {
+        var cartBox = cartBoxes[i];
+        var priceElement = cartBox.querySelector(".cart-price");
+        var quantityElement = cartBox.querySelector(".cart-quantity");
+        var price = parseFloat(priceElement.innerText.replace("$", ""));
+        var quantity = quantityElement.value;
+        total += price * quantity;
+    }
+
+    total = Math.round(total * 100) / 100;
+    document.querySelector(".total-price").innerText = "$" + total;
+}
+
+updatetotal();
